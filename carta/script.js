f8a62f596fc7a12f5f4b9c39e41e8148bbe764cc
Leonardo <154533273+leooosalinas@users.noreply.github.com>
2024-02-20 01:46:40 +0000

    Nuevos cambios y carga paralela de traducciones

--- carta/script.js
+++ carta/script.js
@@ -1,3 +1,35 @@
+function setLanguage(language) {
+  localStorage.setItem('selectedLanguage', language);
+  document.querySelectorAll("[data-translate]").forEach(el => {
+    const key = el.dataset.translate;
+    el.textContent = translations[language][key] || el.textContent;
+  });
+
+  document.querySelectorAll("[data-price]").forEach(el => {
+    const priceKey = el.dataset.price;
+    el.textContent = prices[priceKey] || el.textContent;
+  });
+
+  // Actualizar el estilo del botón activo
+  document.querySelectorAll("#language-selector button").forEach(button => {
+    button.classList.remove('button-active');
+  });
+  document.querySelector(`#btn-${language}`).classList.add('button-active');
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedLanguage = localStorage.getItem('selectedLanguage') || 'es';
+  setLanguage(savedLanguage);
+  document.querySelector(`#btn-${savedLanguage}`).classList.add('button-active');
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Verificar si hay un idioma guardado en el almacenamiento local y establecerlo
+  const savedLanguage = localStorage.getItem('selectedLanguage') || 'es';
+  setLanguage(savedLanguage);
+  document.querySelector('#language-selector select').value = savedLanguage;
+});
+
 function toggleSection(sectionId, element) {
   var section = document.getElementById(sectionId);
   if (section.style.display === "none" || section.style.display === "") {
@@ -18,3 +50,10 @@ document.addEventListener('DOMContentLoaded', function() {
     titles[i].innerHTML = "−"; // Establece el símbolo a "−" ya que todas las secciones están abiertas
   }
 });
+
+window.onload = function() {
+            var logoContainer = document.getElementById('logoContainer');
+            var rotateDirection = Math.random() < 0.5 ? 'rotateLogoRight' : 'rotateLogoLeft';
+            logoContainer.style.animation = rotateDirection + ' 60s linear infinite';
+        };
+
